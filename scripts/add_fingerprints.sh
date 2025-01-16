e8d07da9d69c01a4cfd08bc10a15cff91771974a
William Braeckman <wbr@odoo.com>
2025-01-16 09:28:25 +0100

    fix: add_fingerprints script

--- scripts/add_fingerprints.sh
+++ scripts/add_fingerprints.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
- for i in {102..256}
- do
+for i in {103..256}
+do
+     # The following will remove the old known_host from your ~/.ssh/known_hosts file
+     # This is necessary if some servers get reinstalled.
+     ssh-keygen -q -R runbot$i.odoo.com
      # BatchMode removes the password prompt if we don't own the ip pointed by the DNS record
      # StrictHostKeyChecking=accept-new automatically approves the fingerprint
      # -t 'exit' to close the connection directly
-     ssh root@runbot$i.odoo.com -o BatchMode=yes -O StrictHostKeyChecking=accept-new -t 'exit'
- done
+     ssh root@runbot$i.odoo.com -o BatchMode=yes -o StrictHostKeyChecking=accept-new -t "exit"
+done
